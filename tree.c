2f84469a888050b5680509ae679c30d653739c71
Theo Poette <tpoette@localhost.localdomain>
2019-10-03 17:51:30 +0200

    start-up

---
+++ tree.c
@@ -0,0 +1,48 @@
+/*
+** EPITECH PROJECT, 2019
+** Fir Tree
+** File description:
+** ascii resizable fir tree
+*/
+
+int main(void)
+{
+    tree(1);
+    return(0);
+}
+
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int shape_creation(void)
+{
+    my_putchar(42);
+    my_putchar('\n');
+}
+
+void tree(int size)
+{
+    int i = 0;
+    int *coef_multi = i;
+
+    while (coef_multi < size) {
+        coef_multi++;
+}
+    shape_creation();
+    trunk();
+}
+
+int trunk(void)
+{
+    my_putchar(124);
+    my_putchar('\n');
+}
+
+int new_floor(void)
+{    
+
+
+}
+
